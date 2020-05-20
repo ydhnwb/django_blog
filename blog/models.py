9b7d582df4eb0e1b92ba12cc9f86db4f0880f080
Prieyudha Akadita S <akaditasustono@gmail.com>
2020-05-20 18:20:16 +0700

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ydhnwb/Workstation/Projects/Django/BlogApp/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

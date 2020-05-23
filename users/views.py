3371aaf494881126e14fe18f7fe07742c67a7394
Prieyudha Akadita S <akaditasustono@gmail.com>
2020-05-23 20:13:55 +0700

    read user info

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -18,4 +18,11 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    user_update_form = UserUpdateForm()
+    profile_update_form = ProfileUpdateForm()
+    context =  {
+        'p_form': profile_update_form,
+        'u_form': user_update_form
+    }
+    return render(request, 'users/profile.html', context)
+
